51d2685a15a94aaecd2e2f29738185085688ee0d
Sara Posada <saraposadadominguez@gmail.com>
2026-06-12 12:10:10 +0100

    Update t3e2conversordemonedas.py

--- ejercicios-sara/t3e2conversordemonedas.py
+++ ejercicios-sara/t3e2conversordemonedas.py
@@ -1,6 +1,3 @@
-# Solicitamos a la usuaria una cantidad en euros
-cantidad_en_euros= float(input("Cantidad en euros: "))
-
 # Función que reciba la cantidad en euros y devuelva el total en dólares (1 euro = 1.1 dólares)
 def convertir_a_dolares(euros):
     return euros * 1.1
@@ -8,6 +5,9 @@ def convertir_a_dolares(euros):
 # Función que reciba la cantidad en euros y devuelva el total en libras (1 euro = 0.87 libras)
 def convertir_a_libras(euros):
     return euros * 0.87
+
+# Solicitamos a la usuaria una cantidad en euros
+cantidad_en_euros= float(input("Cantidad en euros: "))
 
 # Muestra la cantidad en euros, en dólares y en libras
 dolares = convertir_a_dolares(cantidad_en_euros)
