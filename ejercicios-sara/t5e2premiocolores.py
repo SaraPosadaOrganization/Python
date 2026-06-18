b7e378579c4bf55b45d4d6ce0e6d3a8ccb859129
Sara Posada <saraposadadominguez@gmail.com>
2026-06-18 12:31:52 +0100

    Update t5e2premiocolores.py

--- ejercicios-sara/t5e2premiocolores.py
+++ ejercicios-sara/t5e2premiocolores.py
@@ -1,7 +1,12 @@
+# Pedir a usuaria 5 veces que introduzca un color
 for i in range(5):
-    color = input("Introduce un color: ")
+    color = input("Introduce un color: ").lower()
 
     if color == "azul":
         print("¡Premio conseguido!")
+        break
     else:
-        print("Prueba otro color")
+        if i < 4:
+            print("Prueba otro color")
+       
+    
